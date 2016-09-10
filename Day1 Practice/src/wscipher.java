651050555035318fe17da6a0e4c288a9634980dc
SalomonSmeke <ssmeke@luc.edu>
2016-09-10 14:52:18 -0500

    First day

---
+++ Day1 Practice/src/wscipher.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class wscipher {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+				
+		
+		
+		int arot = sc.nextInt(); 
+		int crot = sc.nextInt();
+		int brot = sc.nextInt();
+
+		while (arot+brot+crot != 0) {
+			
+			
+			arot = sc.nextInt();
+			brot = sc.nextInt();
+			crot = sc.nextInt();
+		}
+
+	}
+
+}
